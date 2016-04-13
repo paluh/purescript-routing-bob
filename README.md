676bf64ed0c0dfa450bc53ff4d85c02ca2990243
Tomasz Rybarczyk <paluho@gmail.com>
2016-04-13 16:30:40 +0200

    README.md + some simple bobs

---
+++ README.md
@@ -0,0 +1,17 @@
+# purescript-routing-bob
+
+Let me introduce Bob The Router (he is nearly a pendulum bob) - simple (but usable) example of partial bidirectional ~~bobs~~ routes generator based on __purescript-boomerang__ and purescript generics.
+
+## Installation
+
+```shell
+bower install purescript-routing-bob
+```
+
+## Limitations
+
+It is called `bob`, because it contains arbitrary, nonconfigurable and partial routes generators based on `Generic` instances.
+When it will grow up maybe it will became `purescript-boomerang-routing`.
+
+Currently you can generate routes only for some subset of purescript types - if you want extend this set and have proposition how to encode others I'm really open to merge pull requests.
+
---
+++ src/Routing/Bob.purs
@@ -0,0 +1,101 @@
+module Routing.Boob where
+
+import Control.Monad.Eff.Exception.Unsafe (unsafeThrow)
+import Debug.Trace (trace)
+import Data.Array as Array
+import Data.Array.Unsafe (head)
+import Data.Foldable (foldl, foldr)
+import Data.Generic (class Generic, DataConstructor, fromSpine, GenericSignature(..),
+                     GenericSpine(..), toSignature, toSpine)
+import Data.List (fromFoldable, List)
+import Data.Maybe (Maybe(..))
+import Prelude (const, id, show, unit, Unit, (<<<), (<>), (==), (<$>), ($))
+import Text.Boomerang.Combinators (cons, maph, nil, pure)
+import Text.Boomerang.HStack (HCons, hNil, HNil)
+import Text.Boomerang.String (int, lit, noneOf, StringBoomerang, string)
+import Type.Proxy (Proxy)
+
+join :: forall a b c. StringBoomerang b c -> StringBoomerang a b -> StringBoomerang a c
+join b1 b2 = b1 <<< lit "/" <<< b2
+
+infixl 6 join as </>
+
+boolean :: forall r. StringBoomerang r (HCons Boolean r)
+boolean =
+  boolean' <<< (string "on" <> string "off")
+ where
+  boolean' :: forall s. StringBoomerang (HCons String s) (HCons Boolean s)
+  boolean' =
+    maph prs ser
+   where
+    prs "on" = true
+    prs _    = false
+
+    ser true  = Just "on"
+    ser false = Just "off"
+
+arrayFromList :: forall t a tok. Boomerang tok (HCons (List a) t) (HCons (Array a) t)
+arrayFromList =
+  maph arrayFromFoldable (Just <<< fromFoldable)
+ where
+  -- very ineficient - will be replaced with next purescript-array release
+  arrayFromFoldable = foldr Data.Array.cons []
+
+
+signatureToSpineBoomerang :: forall r. GenericSignature -> StringBoomerang r (HCons GenericSpine r)
+signatureToSpineBoomerang SigBoolean =
+  maph SBoolean ser <<< boolean
+ where
+  ser (SBoolean b) = Just b
+  ser _            = Nothing
+signatureToSpineBoomerang SigInt =
+  maph SInt ser <<< int
+ where
+  ser (SInt b) = Just b
+  ser _        = Nothing
+signatureToSpineBoomerang (SigRecord props) =
+  toRecord <<< foldl step nil props
+ where
+  step r e = cons </> (toProp e.recLabel <<< signatureToSpineBoomerang (e.recValue unit)) <<< r
+
+  toProp :: forall s. String -> StringBoomerang (HCons GenericSpine s) (HCons GenericRecProp s)
+  toProp l =
+    maph propPrs propSer
+   where
+    propPrs v = {recLabel : l, recValue : \_ -> v}
+    propSer p =
+      trace ("p.recLabel = " <> p.recLabel <> "; l = " <> l)
+        (\_ ->
+          if p.recLabel == l
+            then Just (p.recValue unit)
+            else Nothing)
+  toRecord :: forall s. StringBoomerang (HCons (List GenericRecProp) s) (HCons GenericSpine s)
+  toRecord =
+    maph SRecord recSer <<< arrayFromList
+   where
+    recSer (SRecord props) = Just props
+    recSer _               = Nothing
+signatureToSpineBoomerang s@(SigProd n cs) =
+  -- XXX: this is coorectly only for single constructor
+  let constructor = (head cs) in
+  fromConstructor constructor
+ where
+  fromConstructor :: forall s. DataConstructor -> StringBoomerang s (HCons GenericSpine s)
+  fromConstructor constructor =
+    maph (SProd constructor.sigConstructor) ser <<< arrayFromList <<< foldl step nil constructor.sigValues
+   where
+    ser (SProd c values) = Just values
+    ser _                = Nothing
+
+    lazy :: forall a t. StringBoomerang (HCons a t) (HCons (Unit -> a) t)
+    lazy = maph const (Just <<< (_ $ unit))
+
+    step r e = cons </> lazy <<< signatureToSpineBoomerang (e unit) <<< r
+
+boob :: forall a r. (Generic a) => Proxy a -> StringBoomerang r (HCons a r)
+boob p =
+  maph prs (\v -> Just (Just v)) <<< maph fromSpine (toSpine <$> _) <<< (signatureToSpineBoomerang (toSignature p))
+ where
+  prs (Just s) = s
+  prs Nothing = unsafeThrow ("Incorrect spine generated for signature: " <> show (toSignature p))
+
