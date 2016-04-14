e2ab8a1975f57f97b84bb0e8a6173809ff0db36f
Tomasz Rybarczyk <paluho@gmail.com>
2016-04-15 01:28:45 +0200

    Code cleanup

--- src/Routing/Bob.purs
+++ src/Routing/Bob.purs
@@ -6,13 +6,14 @@ import Data.Array as Array
 import Data.Array (length)
 import Data.Array.Unsafe (head, tail)
 import Data.List as List
+import Data.List (List(..), (:))
 import Data.Foldable (foldl, foldr)
 import Data.Generic (class Generic, DataConstructor, fromSpine, GenericSignature(..),
                      GenericSpine(..), toSignature, toSpine)
 import Data.List (fromFoldable, List)
 import Data.Maybe (fromMaybe, Maybe(..))
 import Data.String (split, toLower)
-import Prelude (bind, const, id, return, show, unit, Unit, (<<<), (<>), (==), (<$>), ($))
+import Prelude (bind, const, id, map, return, show, unit, Unit, (<<<), (<>), (==), (<$>), ($), (>))
 import Text.Boomerang.Combinators (cons, maph, nil)
 import Text.Boomerang.HStack (HCons)
 import Text.Boomerang.Prim (Boomerang)
@@ -47,7 +48,50 @@ arrayFromList =
 
 type GenericRecProp = {recLabel :: String, recValue :: Unit -> GenericSpine}
 
+
+intersperce :: forall tok a t. (List (Boomerang tok (HCons (List a) t) (HCons a (HCons (List a) t)))) ->
+                               (forall r. Boomerang tok r r) ->
+                               Boomerang tok t (HCons (List a) t)
+intersperce Nil _ = nil
+intersperce (Cons b t) sep =
+  cons <<< b <<< foldr step nil t
+ where
+  step e l = sep <<< cons <<< e <<< l
+
 signatureToSpineBoomerang :: forall r. GenericSignature -> Maybe (StringBoomerang r (HCons GenericSpine r))
+signatureToSpineBoomerang s@(SigProd n cs) = do
+  { head : h, tail: t} <- Array.uncons cs
+  if length t == 0
+    then
+      fromConstructor h false
+    else do
+      hs <- fromConstructor h true
+      Array.foldM (\r c -> (_ <> r) <$> (fromConstructor c true)) hs t
+ where
+  fromConstructor constructor prependWithConstructorName = do
+    let values = map (_ $ unit) constructor.sigValues
+    valuesSpines <- Array.foldM (\r e -> (\b -> (lazy <<< b) : r) <$> signatureToSpineBoomerang e) Nil values
+    let bmg = maph (SProd constructor.sigConstructor) ser <<< arrayFromList <<< intersperce valuesSpines (lit "/")
+    if prependWithConstructorName
+      then do
+        let cn = serializeConstructorName constructor.sigConstructor
+        if length values == 0
+          then return (lit cn <<< bmg)
+          else return (lit cn </> bmg)
+      else return bmg
+   where
+    lazy :: forall z y. StringBoomerang (HCons y z) (HCons (Unit -> y) z)
+    lazy = maph const (Just <<< (_ $ unit))
+
+    ser (SProd c values) =
+      if c == constructor.sigConstructor
+        then Just values
+        else Nothing
+    ser _                = Nothing
+
+    serializeConstructorName n =
+      fromMaybe n (Array.last (split "." <<< toLower $ n))
+
 signatureToSpineBoomerang SigBoolean =
   Just (maph SBoolean ser <<< boolean)
  where
@@ -58,55 +102,6 @@ signatureToSpineBoomerang SigInt =
  where
   ser (SInt b) = Just b
   ser _        = Nothing
-signatureToSpineBoomerang s@(SigProd n cs) = do
-  { head : h, tail: t} <- Array.uncons cs
-  if length t == 0
-    then
-      fromConstructor h false
-    else do
-      hs <- fromConstructor h true
-      Array.foldM constructorStep hs t
- where
-  constructorStep r c = do
-    s <- (fromConstructor c true)
-    return (s <> r)
-
-  fromConstructor :: forall s. DataConstructor -> Boolean -> Maybe (StringBoomerang s (HCons GenericSpine s))
-  fromConstructor constructor prependWithConstructorName = do
-    av <- appendValues
-    let bmg = maph (SProd constructor.sigConstructor) ser <<< arrayFromList <<< av <<< nil
-    return
-      if prependWithConstructorName
-        then lit (serializeConstructorName constructor.sigConstructor) <<< bmg
-        else bmg
-   where
-
-    appendValues = case Array.uncons constructor.sigValues of
-      Just { head: h, tail: t } -> do
-        h' <- valueStep h
-        t' <- Array.foldM (\r e -> do
-          e' <- valueStep e
-          return (e' </> r)) h' t
-        return
-          if prependWithConstructorName
-            then (lit "/" <<< t')
-            else t'
-      Nothing -> Just id
-     where
-      valueStep e =
-        (\t -> cons <<< lazy <<< t) <$> signatureToSpineBoomerang (e unit)
-
-    ser (SProd c values) =
-      if c == constructor.sigConstructor
-        then Just values
-        else Nothing
-    ser _                = Nothing
-
-    lazy :: forall a t. StringBoomerang (HCons a t) (HCons (Unit -> a) t)
-    lazy = maph const (Just <<< (_ $ unit))
-
-  serializeConstructorName n =
-    fromMaybe n (Array.last (split "." <<< toLower $ n))
 signatureToSpineBoomerang _ = Nothing
 
 bob :: forall a r. (Generic a) => Proxy a -> Maybe (StringBoomerang r (HCons a r))
