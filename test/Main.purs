83097d5594a33f820a06e941b57f6a22a96df145
Tomasz Rybarczyk <paluho@gmail.com>
2016-08-10 15:27:16 +0200

    Let's test pass

--- test/Main.purs
+++ test/Main.purs
@@ -8,8 +8,9 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
 import Data.Either (Either(Left))
 import Data.Generic (gShow, class Generic, gEq)
+import Data.List (List(Nil), (:))
 import Data.Maybe (Maybe(..))
-import Data.StrMap (fromFoldable)
+import Data.StrMap (fromList, StrMap, empty, fromFoldable)
 import Data.Tuple (Tuple(Tuple))
 import Routing.Bob (boolean, UrlBoomerang, param, UrlBoomerangToken, Router, fromUrl, toUrl, genericFromUrl, genericToUrl, router)
 import Test.Unit (suite, failure, test, TIMER)
@@ -17,8 +18,8 @@ import Test.Unit.Assert (equal)
 import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Main (runTest)
 import Text.Boomerang.Combinators (pureBmg)
-import Text.Boomerang.HStack (hCons, hArg, hNil, hHead, HCons(HCons), HNil(HNil))
-import Text.Boomerang.Prim (Boomerang(Boomerang))
+import Text.Boomerang.HStack (hSingleton, hCons, hArg, hNil, hHead, HCons(HCons), HNil(HNil))
+import Text.Boomerang.Prim (runSerializer, Boomerang(Boomerang))
 import Text.Boomerang.String (int, StringBoomerang, manyNoneOf)
 import Text.Parsing.Parser (ParseError(ParseError), runParser)
 import Text.Parsing.Parser.Pos (initialPos)
@@ -180,19 +181,30 @@ main = runTest $ suite "Test" do
       equal ("this%2Fis%3Ftest%23string") (toUrl r obj)
       equal (Just obj) (fromUrl r "this%2Fis%3Ftest%23string"))
 
-  test "single param parsing" do
-    let
-      query = fromFoldable [Tuple "param" "somevalue"]
-      url = { path: "/", query: query }
-      urlBmg = param "param" (manyNoneOf "")
-    equal Nothing (parse urlBmg url)
+  suite "Query parsing" do
+    test "single param parsing" do
+      let
+        query = fromFoldable [Tuple "param" "somevalue"]
+        urlBmg = param "param" (manyNoneOf "")
+        url = { path: "/", query: query }
+      equal
+        (Just "somevalue")
+        (parse urlBmg url)
 
-  test "multiple parameters parsing" do
-    let
-      query = fromFoldable [Tuple "paramInt" "8", Tuple "paramBoolean" "off", Tuple "paramString" "somestringvalue"]
-      url = { path: "/", query: query }
-      urlBmg = multipleParams
-    equal Nothing (parse multipleParams url)
+    test "multiple parameters parsing" do
+      let
+        query = fromFoldable [Tuple "paramInt" "8", Tuple "paramBoolean" "off", Tuple "paramString" "somestringvalue"]
+        url = { path: "/", query: query }
+      equal
+        (parse multipleParams url)
+        (Just $ params "somestringvalue" 8 false)
+
+  suite "Query serializing" do
+    test "serialization" do
+      equal
+        (Just <<< fromList $ (Tuple "paramBoolean" "off") : (Tuple "paramInt" "8") : (Tuple "paramString" "tes") : Nil)
+        (_.query <$> (serialize multipleParams (params "tes" 8 false)))
+
 
 
 newtype Params =
@@ -204,6 +216,9 @@ newtype Params =
 derive instance genericParms :: Generic Params
 derive instance eqParams :: Eq Params
 
+params :: String -> Int -> Boolean -> Params
+params = ((Params <$> _) <$>  _) <$> {paramString: _, paramInt: _, paramBoolean: _ }
+
 instance showParams :: Show Params where
   show = gShow
 
@@ -213,7 +228,7 @@ multipleParams =
  where
   rBmg =
     pureBmg
-      (hArg (hArg (hArg hCons)) (((Params <$> _) <$>  _) <$> {paramString: _, paramInt: _, paramBoolean: _ }))
+      (hArg (hArg (hArg hCons)) params)
       (\(HCons (Params r) t) -> Just (hCons r.paramString (hCons r.paramInt (hCons r.paramBoolean t))))
 
 -- any :: StringBoomerang r (HCons String r)
@@ -228,3 +243,8 @@ parse (Boomerang b) s = do
     -- eof
     pure r))
   pure (hHead (f hNil))
+
+serialize :: forall a. UrlBoomerang HNil (HCons a HNil) -> a -> Maybe UrlBoomerangToken
+serialize (Boomerang b) s = do
+  (Tuple f _) <- runSerializer b.ser (hSingleton s)
+  pure (f ({ path: "", query: empty }))
